0a5add55871797566a71e54d51913772c5c4a0e9
Jon Johnson <jonjohnson@google.com>
2018-05-12 01:32:38 +0000

    Implement remote.List

---
+++ cmd/crane/list.go
@@ -0,0 +1,56 @@
+// Copyright 2018 Google LLC All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"net/http"
+
+	"fmt"
+	"log"
+
+	"github.com/google/go-containerregistry/authn"
+	"github.com/google/go-containerregistry/name"
+	"github.com/google/go-containerregistry/v1/remote"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "ls",
+		Short: "List the tags in a repo",
+		Args:  cobra.ExactArgs(1),
+		Run:   ls,
+	})
+}
+
+func ls(_ *cobra.Command, args []string) {
+	r := args[0]
+	repo, err := name.NewRepository(r, name.WeakValidation)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	auth, err := authn.DefaultKeychain.Resolve(repo.Registry)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	tags, err := remote.List(repo, auth, http.DefaultTransport)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, tag := range tags {
+		fmt.Println(tag)
+	}
+}
