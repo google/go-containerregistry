5a00d0bdc3e56393f292cecf156005e04bc14f25
sharifelgamal <selgamal@google.com>
2018-06-21 14:45:30 -0700

    preserve config for reproducible images

--- pkg/v1/mutate/mutate.go
+++ pkg/v1/mutate/mutate.go
@@ -411,13 +411,23 @@ func Time(img v1.Image, t time.Time) (v1.Image, error) {
 		return nil, fmt.Errorf("Error appending layers: %v", err)
 	}
 
-	// Strip away timestamps from the config file
+	// Copy basic config over
+	ocf, err := img.ConfigFile()
+	if err != nil {
+		return nil, fmt.Errorf("Error getting original config file: %v", err)
+	}
+
 	cf, err := newImage.ConfigFile()
 	if err != nil {
 		return nil, fmt.Errorf("Error setting config file: %v", err)
 	}
 
 	cfg := cf.DeepCopy()
+
+	cfg.Config = ocf.Config
+	cfg.ContainerConfig = ocf.ContainerConfig
+
+	// Strip away timestamps from the config file
 	cfg.Created = v1.Time{Time: t}
 
 	for _, h := range cfg.History {
