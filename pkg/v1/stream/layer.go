430ca30659fc1e621a9d61aad7a0a43eedfd0a46
Jason Hall <jasonhall@google.com>
2018-11-11 13:59:12 -0500

    Add new stream.Layer which actually passes tests now

---
+++ pkg/v1/stream/layer.go
@@ -0,0 +1,142 @@
+package stream
+
+import (
+	"compress/gzip"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"hash"
+	"io"
+
+	"github.com/google/go-containerregistry/pkg/v1"
+)
+
+// ErrNotComputed is returned when the requested value is not yet computed
+// because the stream has not been consumed yet.
+var ErrNotComputed = errors.New("value not computed until stream is consumed")
+
+type Layer struct {
+	blob io.ReadCloser
+
+	digest, diffID *v1.Hash
+	size           int64
+}
+
+var _ v1.Layer = (*Layer)(nil)
+
+func NewLayer(rc io.ReadCloser) *Layer { return &Layer{blob: rc} }
+
+func (l *Layer) Digest() (v1.Hash, error) {
+	if l.digest == nil {
+		return v1.Hash{}, ErrNotComputed
+	}
+	return *l.digest, nil
+}
+
+func (l *Layer) DiffID() (v1.Hash, error) {
+	if l.diffID == nil {
+		return v1.Hash{}, ErrNotComputed
+	}
+	return *l.diffID, nil
+}
+
+func (l *Layer) Size() (int64, error) {
+	if l.size == 0 {
+		return 0, ErrNotComputed
+	}
+	return l.size, nil
+}
+
+func (l *Layer) Uncompressed() (io.ReadCloser, error) {
+	return nil, errors.New("NYI: stream.Layer.Uncompressed is not implemented")
+}
+
+func (l *Layer) Compressed() (io.ReadCloser, error) { return newCompressedReader(l) }
+
+type compressedReader struct {
+	closer io.Closer // original blob's Closer.
+
+	h, zh hash.Hash // collects digests of compressed and uncompressed stream.
+	pr    io.Reader
+	count *countWriter
+
+	l *Layer // stream.Layer to update upon Close.
+}
+
+func newCompressedReader(l *Layer) (*compressedReader, error) {
+	h := sha256.New()
+	zh := sha256.New()
+	count := &countWriter{}
+
+	// gzip.Writer writes to the output stream, a hash calculator to
+	// capture compressed digest, and a countWriter to capture compressed
+	// size.
+	zw, err := gzip.NewWriterLevel(io.MultiWriter(zh, count), gzip.BestSpeed)
+	if err != nil {
+		return nil, err
+	}
+
+	// Pipe the contents of rc through
+	pr, pw := io.Pipe()
+	go func() {
+		_, err := io.Copy(pw, io.TeeReader(l.blob, io.MultiWriter(h, zw)))
+		pw.CloseWithError(err)
+	}()
+
+	return &compressedReader{
+		closer: newMultiCloser(zw, l.blob),
+		pr:     pr,
+		h:      h,
+		zh:     zh,
+		count:  count,
+		l:      l,
+	}, nil
+}
+
+// multiCloser is a Closer that collects multiple Closers and Closes them in order.
+type multiCloser []io.Closer
+
+var _ io.Closer = (multiCloser)(nil)
+
+func newMultiCloser(c ...io.Closer) multiCloser { return multiCloser(c) }
+
+func (m multiCloser) Close() error {
+	for _, c := range m {
+		if err := c.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (cr *compressedReader) Read(b []byte) (int, error) { return cr.pr.Read(b) }
+
+func (cr *compressedReader) Close() error {
+	// Close the inner ReadCloser.
+	if err := cr.closer.Close(); err != nil {
+		return err
+	}
+
+	diffID, err := v1.NewHash("sha256:" + hex.EncodeToString(cr.h.Sum(nil)))
+	if err != nil {
+		return err
+	}
+	cr.l.diffID = &diffID
+
+	digest, err := v1.NewHash("sha256:" + hex.EncodeToString(cr.zh.Sum(nil)))
+	if err != nil {
+		return err
+	}
+	cr.l.digest = &digest
+
+	cr.l.size = cr.count.n
+	return nil
+}
+
+// countWriter counts bytes written to it.
+type countWriter struct{ n int64 }
+
+func (c *countWriter) Write(p []byte) (int, error) {
+	c.n += int64(len(p))
+	return len(p), nil
+}
---
+++ pkg/v1/stream/stream_test.go
@@ -0,0 +1,166 @@
+package stream
+
+import (
+	"archive/tar"
+	"bytes"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"testing"
+
+	"github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
+)
+
+func TestStreamVsBuffer(t *testing.T) {
+	n := 10000
+	newBlob := func() io.ReadCloser { return ioutil.NopCloser(bytes.NewReader(bytes.Repeat([]byte{'a'}, n))) }
+	wantDigest := "sha256:3d7c465be28d9e1ed810c42aeb0e747b44441424f566722ba635dc93c947f30e"
+	wantDiffID := "sha256:27dd1f61b867b6a0f6e9d8a41c43231de52107e53ae424de8f847b821db4b711"
+	var wantSize int64 = 49
+
+	// Check that streaming some content results in the expected digest/diffID/size.
+	l := NewLayer(newBlob())
+	if c, err := l.Compressed(); err != nil {
+		t.Errorf("Compressed: %v", err)
+	} else {
+		if _, err := io.Copy(ioutil.Discard, c); err != nil {
+			t.Errorf("error reading Compressed: %v", err)
+		}
+		if err := c.Close(); err != nil {
+			t.Errorf("Close: %v", err)
+		}
+	}
+	if d, err := l.Digest(); err != nil {
+		t.Errorf("Digest: %v", err)
+	} else if d.String() != wantDigest {
+		t.Errorf("stream Digest got %q, want %q", d.String(), wantDigest)
+	}
+	if d, err := l.DiffID(); err != nil {
+		t.Errorf("DiffID: %v", err)
+	} else if d.String() != wantDiffID {
+		t.Errorf("stream DiffID got %q, want %q", d.String(), wantDiffID)
+	}
+	if s, err := l.Size(); err != nil {
+		t.Errorf("Size: %v", err)
+	} else if s != wantSize {
+		t.Errorf("stream Size got %d, want %d", s, wantSize)
+	}
+
+	// Test that buffering the same contents and using
+	// tarball.LayerFromOpener results in the same digest/diffID/size.
+	tl, err := tarball.LayerFromOpener(func() (io.ReadCloser, error) { return newBlob(), nil })
+	if err != nil {
+		t.Fatalf("LayerFromOpener: %v", err)
+	}
+	if d, err := tl.Digest(); err != nil {
+		t.Errorf("Digest: %v", err)
+	} else if d.String() != wantDigest {
+		t.Errorf("tarball Digest got %q, want %q", d.String(), wantDigest)
+	}
+	if d, err := tl.DiffID(); err != nil {
+		t.Errorf("DiffID: %v", err)
+	} else if d.String() != wantDiffID {
+		t.Errorf("tarball DiffID got %q, want %q", d.String(), wantDiffID)
+	}
+	if s, err := tl.Size(); err != nil {
+		t.Errorf("Size: %v", err)
+	} else if s != wantSize {
+		t.Errorf("stream Size got %d, want %d", s, wantSize)
+	}
+}
+
+// TestStreamZero tests that Digest/DiffID/Size return ErrNotComputed before
+// the stream has been consumed.
+func TestStreamZero(t *testing.T) {
+	l := NewLayer(ioutil.NopCloser(bytes.NewBufferString("hi")))
+
+	// All methods should return ErrNotComputed until the stream has been
+	// consumed and closed.
+	if _, err := l.Size(); err != ErrNotComputed {
+		t.Errorf("Size: got %v, want %v", err, ErrNotComputed)
+	}
+	if _, err := l.Digest(); err == nil {
+		t.Errorf("Digest: got %v, want %v", err, ErrNotComputed)
+	}
+	if _, err := l.DiffID(); err == nil {
+		t.Errorf("DiffID: got %v, want %v", err, ErrNotComputed)
+	}
+}
+
+func TestLargeStream(t *testing.T) {
+	var n, wantSize int64 = 100000000, 100007653 // "Compressing" n random bytes results in this many bytes.
+	sl := NewLayer(ioutil.NopCloser(io.LimitReader(rand.Reader, n)))
+	rc, err := sl.Compressed()
+	if err != nil {
+		t.Fatalf("Uncompressed: %v", err)
+	}
+	if _, err := io.Copy(ioutil.Discard, rc); err != nil {
+		t.Fatalf("Reading layer: %v", err)
+	}
+	if err := rc.Close(); err != nil {
+		t.Fatalf("Close: %v", err)
+	}
+
+	if dig, err := sl.Digest(); err != nil {
+		t.Errorf("Digest: %v", err)
+	} else if dig.String() == (v1.Hash{}).String() {
+		t.Errorf("Digest got %q, want anything else", (v1.Hash{}).String())
+	}
+	if diffID, err := sl.DiffID(); err != nil {
+		t.Errorf("DiffID: %v", err)
+	} else if diffID.String() == (v1.Hash{}).String() {
+		t.Errorf("DiffID got %q, want anything else", (v1.Hash{}).String())
+	}
+	if size, err := sl.Size(); err != nil {
+		t.Errorf("Size: %v", err)
+	} else if size != wantSize {
+		t.Errorf("Size got %d, want %d", size, n)
+	}
+}
+
+func TestStreamableLayerFromTarball(t *testing.T) {
+	pr, pw := io.Pipe()
+	tw := tar.NewWriter(pw)
+	go func() {
+		// "Stream" a bunch of files into the layer.
+		pw.CloseWithError(func() error {
+			for i := 0; i < 1000; i++ {
+				name := fmt.Sprintf("file-%d.txt", i)
+				body := fmt.Sprintf("i am file number %d", i)
+				if err := tw.WriteHeader(&tar.Header{
+					Name:     name,
+					Mode:     0600,
+					Size:     int64(len(body)),
+					Typeflag: tar.TypeReg,
+				}); err != nil {
+					return err
+				}
+				if _, err := tw.Write([]byte(body)); err != nil {
+					return err
+				}
+			}
+			return nil
+		}())
+	}()
+
+	l := NewLayer(pr)
+	rc, err := l.Compressed()
+	if err != nil {
+		t.Fatalf("Compressed: %v", err)
+	}
+	if _, err := io.Copy(ioutil.Discard, rc); err != nil {
+		t.Fatalf("Copy: %v", err)
+	}
+	if err := rc.Close(); err != nil {
+		t.Fatalf("Close: %v", err)
+	}
+
+	wantDigest := "sha256:f53d6a164ab476294212843f267740bd12f79e00abd8050c24ce8a9bceaa36b0"
+	if got, err := l.Digest(); err != nil {
+		t.Errorf("Digest: %v", err)
+	} else if got.String() != wantDigest {
+		t.Errorf("Digest: got %q, want %q", got.String(), wantDigest)
+	}
+}
