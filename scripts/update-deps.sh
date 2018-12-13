d1ad8c8ce2a1445e5ea4bb93c70dcf1603d49fab
Jason Hall <jasonhall@google.com>
2018-12-13 11:51:29 -0800

    Delete broken symlinks, which confuse gcloud builds submit (#321)
    * Delete broken symlinks which confuse gcloud builds submit
    
    * ko publish -P
    
    * Update update-deps.sh

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -24,3 +24,8 @@ pushd ${PROJECT_ROOT}
 trap popd EXIT
 
 dep ensure
+
+# Delete all vendored broken symlinks.
+# From https://stackoverflow.com/questions/22097130/delete-all-broken-symbolic-links-with-a-line
+find vendor/ -type l -exec sh -c 'for x; do [ -e "$x" ] || rm "$x"; done' _ {} +
+
