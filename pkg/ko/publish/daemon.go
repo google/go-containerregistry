92d15587f3196479b93b230004213b8ad2982777
Gustavo Paiva <guustavo.paiva@gmail.com>
2019-01-15 18:36:55 -0200

    publishing image with digest tag too

--- pkg/ko/publish/daemon.go
+++ pkg/ko/publish/daemon.go
@@ -49,13 +49,16 @@ func (d *demon) Publish(img v1.Image, s string) (name.Reference, error) {
 		return nil, err
 	}
 
-	for _, tagName := range d.tags {
+	tagsAndDigest := append(d.tags, h.Hex)
+
+	for _, tagName := range tagsAndDigest {
 		tag, err := name.NewTag(fmt.Sprintf("%s/%s:%s", LocalDomain, d.namer(s), tagName), name.WeakValidation)
 		if err != nil {
 			return nil, err
 		}
 		log.Printf("Loading %v", tag)
-		// TODO: This is slow because we have to load the image multiple times. We should use client.ImageTag instead
+		// TODO: This is slow because we have to load the image multiple times.
+		// We should use client.ImageTag instead
 		if _, err := daemon.Write(tag, img); err != nil {
 			return nil, err
 		}
