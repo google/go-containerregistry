440ab0ed66188f3196ce16c2a780be9bee10e0ad
Brian de Alwis <bsd@mt.ca>
2019-02-12 21:50:09 -0500

    Accept 0-byte layer.tar files

--- pkg/v1/v1util/zip_test.go
+++ pkg/v1/v1util/zip_test.go
@@ -40,3 +40,26 @@ func TestReader(t *testing.T) {
 		t.Errorf("ReadAll(); got %q, want %q", got, want)
 	}
 }
+
+func TestIsGzipped(t *testing.T) {
+	tests := []struct {
+		in  []byte
+		out bool
+		err error
+	}{
+		{[]byte{}, false, nil},
+		{[]byte{'\x00', '\x00', '\x00'}, false, nil},
+		{[]byte{'\x1f', '\x8b', '\x1b'}, true, nil},
+	}
+	for _, test := range tests {
+		reader := bytes.NewReader(test.in)
+		got, err := IsGzipped(reader)
+		if got != test.out {
+			t.Errorf("IsGzipped; n: got %v, wanted %v\n", got, test.out)
+		}
+		if err != test.err {
+			t.Errorf("IsGzipped; err: got %v, wanted %v\n", err, test.err)
+		}
+	}
+}
+
