fba3c08be426c4a60001b1069937ffce6bb8c39b
Jon Johnson <jonjohnson@google.com>
2019-03-13 17:43:11 -0700

    Add --all-tags and --oci to crane push/pull
    This is a demo to work out any kinks with the OCI image layout library.
    There are some TODOs we will probably want to address.

--- pkg/crane/pull.go
+++ pkg/crane/pull.go
@@ -17,9 +17,12 @@ package crane
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/empty"
+	"github.com/google/go-containerregistry/pkg/v1/layout"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/google/go-containerregistry/pkg/v1/tarball"
 	"github.com/spf13/cobra"
@@ -34,17 +37,33 @@ func init() { Root.AddCommand(NewCmdPull()) }
 
 // NewCmdPull creates a new cobra.Command for the pull subcommand.
 func NewCmdPull() *cobra.Command {
-	return &cobra.Command{
+	allTags := false
+	oci := false
+	cmd := &cobra.Command{
 		Use:   "pull",
 		Short: "Pull a remote image by reference and store its contents in a tarball",
 		Args:  cobra.ExactArgs(2),
-		Run:   pull,
+		Run: func(cc *cobra.Command, args []string) {
+			pull(args[0], args[1], allTags, oci)
+		},
 	}
+
+	cmd.Flags().BoolVarP(&allTags, "all-tags", "a", false, "Whether to pull all tags in a repo")
+	cmd.Flags().BoolVar(&oci, "oci", false, "Whether to write images as OCI image layout")
+	return cmd
 }
 
-func pull(_ *cobra.Command, args []string) {
-	src, dst := args[0], args[1]
+// TODO: better options.
+func pull(src, dst string, allTags, oci bool) {
+	if allTags && oci {
+		if err := pullLayout(src, dst); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
 
+	// TODO: Make allTags work for tarball
+	// TODO: Make oci work without allTags
 	ref, err := name.ParseReference(src, name.WeakValidation)
 	if err != nil {
 		log.Fatalf("parsing tag %q: %v", src, err)
@@ -76,4 +95,74 @@ func pull(_ *cobra.Command, args []string) {
 	if err := tarball.WriteToFile(dst, tag, i); err != nil {
 		log.Fatalf("writing image %q: %v", dst, err)
 	}
+}
+
+func pullLayout(src, dst string) error {
+	// Open the layout.
+	path, err := layout.FromPath(dst)
+	if err != nil {
+		// TODO: This could be cleaner. We can have an entrypoint that inits the layout
+		// if it does not already exist.
+		path, err = layout.Write(dst, empty.Index)
+		if err != nil {
+			return fmt.Errorf("cannot init layout: %v", err)
+		}
+	}
+
+	// Fetch all the tags from "src" repo.
+	repo, err := name.NewRepository(src, name.WeakValidation)
+	if err != nil {
+		return err
+	}
+
+	auth, err := authn.DefaultKeychain.Resolve(repo.Registry)
+	if err != nil {
+		return fmt.Errorf("getting creds for %q: %v", repo, err)
+	}
+
+	tags, err := remote.List(repo, auth, http.DefaultTransport)
+	if err != nil {
+		return fmt.Errorf("reading tags for %q: %v", repo, err)
+	}
+
+	// For each tag, append the image to the layout with an annotation describing where we pulled it from.
+	// This will be useful when we push this layout to a new repo, since we'll want to reuse the tags.
+	//
+	// TODO: We could possibly dedupe the descriptors so that one manifest could have multiple tags,
+	// but it's fine to have multiple entries for the same image in here, since re-pushing it will be a NOP.
+	for _, tag := range tags {
+		ref, err := name.ParseReference(fmt.Sprintf("%s:%s", repo, tag), name.WeakValidation)
+		if err != nil {
+			return err
+		}
+		annotation := layout.WithAnnotations(map[string]string{
+			"org.opencontainers.image.ref.name": ref.String(),
+		})
+		auth := remote.WithAuthFromKeychain(authn.DefaultKeychain)
+
+		// TODO: We can handle imageindex better once my remote.Descriptor concept lands.
+		// For now, we just do the fallback thing. try in
+		if err := appendIndex(path, ref, auth, annotation); err != nil {
+			if err := appendImage(path, ref, auth, annotation); err != nil {
+				return fmt.Errorf("appending %v: %v", ref, err)
+			}
+		}
+	}
+	return nil
+}
+
+func appendIndex(path layout.Path, ref name.Reference, auth remote.ImageOption, annotation layout.Option) error {
+	idx, err := remote.Index(ref, auth)
+	if err != nil {
+		return fmt.Errorf("pulling %v: %v", ref, err)
+	}
+	return path.AppendIndex(idx, annotation)
+}
+
+func appendImage(path layout.Path, ref name.Reference, auth remote.ImageOption, annotation layout.Option) error {
+	img, err := remote.Image(ref, auth)
+	if err != nil {
+		return fmt.Errorf("pulling %v: %v", ref, err)
+	}
+	return path.AppendImage(img, annotation)
 }
--- pkg/crane/push.go
+++ pkg/crane/push.go
@@ -15,30 +15,49 @@
 package crane
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/layout"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"github.com/google/go-containerregistry/pkg/v1/types"
 	"github.com/spf13/cobra"
 )
+
+const ociNameAnnotation = "org.opencontainers.image.ref.name"
 
 func init() { Root.AddCommand(NewCmdPush()) }
 
 // NewCmdPush creates a new cobra.Command for the push subcommand.
 func NewCmdPush() *cobra.Command {
-	return &cobra.Command{
+	allTags := false
+	oci := false
+	cmd := &cobra.Command{
 		Use:   "push",
 		Short: "Push image contents as a tarball to a remote registry",
 		Args:  cobra.ExactArgs(2),
-		Run:   push,
+		Run: func(cc *cobra.Command, args []string) {
+			push(args[0], args[1], allTags, oci)
+		},
 	}
+
+	cmd.Flags().BoolVarP(&allTags, "all-tags", "a", false, "Whether to pull all tags in a repo")
+	cmd.Flags().BoolVar(&oci, "oci", false, "Whether to write images as OCI image layout")
+	return cmd
 }
 
-func push(_ *cobra.Command, args []string) {
-	src, dst := args[0], args[1]
+func push(src, dst string, allTags, oci bool) {
+	if allTags && oci {
+		if err := pushLayout(src, dst); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
 	t, err := name.NewTag(dst, name.WeakValidation)
 	if err != nil {
 		log.Fatalf("parsing tag %q: %v", dst, err)
@@ -58,4 +77,69 @@ func push(_ *cobra.Command, args []string) {
 	if err := remote.Write(t, i, auth, http.DefaultTransport); err != nil {
 		log.Fatalf("writing image %q: %v", t, err)
 	}
+}
+
+func renameImage(dst, refName string) (name.Reference, error) {
+	srcTag, err := name.NewTag(refName, name.WeakValidation)
+	if err != nil {
+		return nil, err
+	}
+	tag := srcTag.Identifier()
+	return name.NewTag(fmt.Sprintf("%s:%s", dst, tag), name.WeakValidation)
+}
+
+func pushLayout(src, dst string) error {
+	// Open the layout.
+	ii, err := layout.ImageIndexFromPath(src)
+	if err != nil {
+		return fmt.Errorf("cannot open layout: %v", err)
+	}
+
+	index, err := ii.IndexManifest()
+	if err != nil {
+		return err
+	}
+
+	// For each manifest referenced by the layout, we'll parse out the source
+	// from the annotations and use the same tag when we push to the destination.
+	for _, desc := range index.Manifests {
+		// TODO: We could just upload by digest here.
+		refName, ok := desc.Annotations[ociNameAnnotation]
+		if !ok {
+			return fmt.Errorf("expected %q annotation", ociNameAnnotation)
+		}
+
+		// Reswizzle the source image ref to be the "dst" repo + the source tag.
+		ref, err := renameImage(dst, refName)
+		if err != nil {
+			return fmt.Errorf("couldn't rename ref: %v", err)
+		}
+
+		auth, err := authn.DefaultKeychain.Resolve(ref.Context().Registry)
+		if err != nil {
+			return fmt.Errorf("failed to auth %v: %v", ref, err)
+		}
+
+		switch desc.MediaType {
+		case types.OCIImageIndex, types.DockerManifestList:
+			ii, err := ii.ImageIndex(desc.Digest)
+			if err != nil {
+				return err
+			}
+
+			if err := remote.WriteIndex(ref, ii, auth, http.DefaultTransport); err != nil {
+				return err
+			}
+		case types.OCIManifestSchema1, types.DockerManifestSchema2:
+			img, err := ii.Image(desc.Digest)
+			if err != nil {
+				return err
+			}
+			if err := remote.Write(ref, img, auth, http.DefaultTransport); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
