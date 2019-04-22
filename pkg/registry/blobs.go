a35c7458b8e0ee4779ce398c3671d22ab79845da
Colin <clr@google.com>
2019-04-22 14:54:46 -0400

    registry: Create blobs endpoint and handler.

---
+++ pkg/registry/blobs.go
@@ -0,0 +1,110 @@
+package registry
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"strings"
+)
+
+// Returns whether this url should be handled by the blob handler
+// This is complicated because blob is indicated by the trailing path, not the leading path.
+// https://github.com/opencontainers/distribution-spec/blob/master/spec.md#pulling-a-layer
+// https://github.com/opencontainers/distribution-spec/blob/master/spec.md#pushing-a-layer
+func isBlob(req *http.Request) bool {
+	elem := strings.Split(req.URL.Path, "/")
+	elem = elem[1:]
+	if len(elem) < 3 {
+		return false
+	}
+	return elem[len(elem)-2] == "blobs" || (elem[len(elem)-3] == "blobs" &&
+		elem[len(elem)-2] == "uploads")
+}
+
+// blobs
+type blobs struct {
+	// Blobs are content addresses. we store them globally underneath their sha and make no distinctions per image.
+	contents map[string][]byte
+	// Each upload gets a unique id that writes occur to until finalized.
+	uploads map[string][]byte
+}
+
+// https://github.com/opencontainers/distribution-spec/blob/master/spec.md#pulling-a-layer
+// https://github.com/opencontainers/distribution-spec/blob/master/spec.md#pushing-a-layer
+func (b *blobs) handle(resp http.ResponseWriter, req *http.Request) {
+	elem := strings.Split(req.URL.Path, "/")
+	elem = elem[1:]
+	// Must have a path of form /v2/{container}/blobs/{upload,sha256:}
+	if len(elem) < 4 {
+		resp.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	target := elem[len(elem)-1]
+	service := elem[len(elem)-2]
+
+	if req.Method == "HEAD" {
+		b, ok := b.contents[target]
+		if !ok {
+			resp.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		resp.Header().Set("Content-Length", fmt.Sprint(len(b)))
+		resp.Header().Set("Docker-Content-Digest", target)
+		resp.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if req.Method == "GET" {
+		b, ok := b.contents[target]
+		if !ok {
+			resp.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		resp.Header().Set("Content-Length", fmt.Sprint(len(b)))
+		resp.Header().Set("Docker-Content-Digest", target)
+		resp.WriteHeader(http.StatusOK)
+		io.Copy(resp, bytes.NewReader(b))
+		return
+	}
+
+	if req.Method == "POST" && target == "uploads" {
+		id := fmt.Sprint(rand.Int63())
+		resp.Header().Set("Location", fmt.Sprintf("/v2/%s/blobs/uploads/%s",
+			strings.Join(elem[1:len(elem)-2], ","),
+			id))
+		resp.Header().Set("Range", "0-0")
+		resp.WriteHeader(http.StatusAccepted)
+		return
+	}
+
+	if req.Method == "PUT" && service == "uploads" {
+		digest := req.URL.Query().Get("digest")
+		if digest == "" {
+			resp.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		l := &bytes.Buffer{}
+		io.Copy(l, req.Body)
+		rd := sha256.Sum256(l.Bytes())
+		d := "sha256:" + hex.EncodeToString(rd[:])
+		if d != digest {
+			resp.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		b.contents[d] = l.Bytes()
+		resp.Header().Set("Docker-Content-Digest", d)
+		resp.WriteHeader(http.StatusCreated)
+		return
+	}
+
+	resp.WriteHeader(http.StatusBadRequest)
+	return
+}
--- pkg/registry/registry_test.go
+++ pkg/registry/registry_test.go
@@ -1,7 +1,12 @@
 package registry_test
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/google/go-containerregistry/pkg/registry"
@@ -10,27 +15,98 @@ import (
 func TestCalls(t *testing.T) {
 	tcs := []struct {
 		Description string
-		URL         string
-		Code        int
-		Header      map[string]string
+
+		// Request / setup
+		URL     string
+		Digests map[string]string
+
+		// Response
+		Code   int
+		Header map[string]string
+		Method string
+		Body   string
 	}{
 		{
 			Description: "/v2 returns 200",
+			Method:      "GET",
 			URL:         "/v2",
-			Code:        200,
+			Code:        http.StatusOK,
 			Header:      map[string]string{"Docker-Distribution-API-Version": "registry/2.0"},
 		},
 		{
 			Description: "/v2/ returns 200",
+			Method:      "GET",
 			URL:         "/v2/",
-			Code:        200,
+			Code:        http.StatusOK,
 			Header:      map[string]string{"Docker-Distribution-API-Version": "registry/2.0"},
 		},
 		{
 			Description: "/v2/bad returns 404",
+			Method:      "GET",
 			URL:         "/v2/bad",
-			Code:        404,
+			Code:        http.StatusNotFound,
 			Header:      map[string]string{"Docker-Distribution-API-Version": "registry/2.0"},
+		},
+		{
+			Description: "GET non existent blob",
+			Method:      "GET",
+			URL:         "/v2/foo/blobs/sha256:asd",
+			Code:        http.StatusNotFound,
+		},
+		{
+			Description: "HEAD non existent blob",
+			Method:      "HEAD",
+			URL:         "/v2/foo/blobs/sha256:asd",
+			Code:        http.StatusNotFound,
+		},
+		{
+			Description: "GET blob",
+			Digests:     map[string]string{"sha256:2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae": "foo"},
+			Method:      "GET",
+			URL:         "/v2/foo/blobs/sha256:2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae",
+			Code:        http.StatusOK,
+		},
+		{
+			Description: "HEAD blob",
+			Digests:     map[string]string{"sha256:2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae": "foo"},
+			Method:      "HEAD",
+			URL:         "/v2/foo/blobs/sha256:2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae",
+			Code:        http.StatusOK,
+			Header:      map[string]string{"Content-Length": "3"},
+		},
+		{
+			Description: "blob url with no container",
+			Method:      "GET",
+			URL:         "/v2/blobs/sha256:asd",
+			Code:        http.StatusBadRequest,
+		},
+		{
+			Description: "uploadurl",
+			Method:      "POST",
+			URL:         "/v2/foo/blobs/uploads",
+			Code:        http.StatusAccepted,
+			Header:      map[string]string{"Range": "0-0"},
+		},
+		{
+			Description: "upload put missing digest",
+			Method:      "PUT",
+			URL:         "/v2/foo/blobs/uploads/1",
+			Code:        http.StatusBadRequest,
+		},
+		{
+			Description: "upload good digest",
+			Method:      "PUT",
+			URL:         "/v2/foo/blobs/uploads/1?digest=sha256:2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae",
+			Code:        http.StatusCreated,
+			Body:        "foo",
+			Header:      map[string]string{"Docker-Content-Digest": "sha256:2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"},
+		},
+		{
+			Description: "upload bad digest",
+			Method:      "PUT",
+			URL:         "/v2/foo/blobs/uploads/1?digest=sha256:baddigest",
+			Code:        http.StatusBadRequest,
+			Body:        "foo",
 		},
 	}
 
@@ -38,7 +114,37 @@ func TestCalls(t *testing.T) {
 		t.Run(tc.Description, func(t *testing.T) {
 			s := httptest.NewServer(registry.New())
 			defer s.Close()
-			resp, err := s.Client().Get(s.URL + tc.URL)
+
+			for digest, contents := range tc.Digests {
+				u, err := url.Parse(fmt.Sprintf("%s/v2/foo/blobs/uploads/1?digest=%s", s.URL, digest))
+				if err != nil {
+					t.Fatalf("Error parsing %q: %v", s.URL+tc.URL, err)
+				}
+				req := &http.Request{
+					Method: "PUT",
+					URL:    u,
+					Body:   ioutil.NopCloser(strings.NewReader(contents)),
+				}
+				resp, err := s.Client().Do(req)
+				if err != nil {
+					t.Fatalf("Error uploading digest: %v", err)
+				}
+				if resp.StatusCode != http.StatusCreated {
+					t.Fatalf("Error uploading digest got status: %d", resp.StatusCode)
+				}
+
+			}
+
+			u, err := url.Parse(s.URL + tc.URL)
+			if err != nil {
+				t.Fatalf("Error parsing %q: %v", s.URL+tc.URL, err)
+			}
+			req := &http.Request{
+				Method: tc.Method,
+				URL:    u,
+				Body:   ioutil.NopCloser(strings.NewReader(tc.Body)),
+			}
+			resp, err := s.Client().Do(req)
 			if err != nil {
 				t.Fatalf("Error getting %q: %v", tc.URL, err)
 			}
