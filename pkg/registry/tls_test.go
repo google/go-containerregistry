ee6d6c952765107429656840e4dd5f19f48dadac
Colin <clr@google.com>
2019-07-23 14:22:30 -0400

    Create a tls version of the registry.New()
    This handles the complexity of generating certs and forcing transports
    to the server with the correct root ca. This should be usable for
    replacing any given container registry in a test.

---
+++ pkg/registry/tls_test.go
@@ -0,0 +1,34 @@
+package registry
+
+import (
+	"testing"
+
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/random"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+)
+
+func TestTLS(t *testing.T) {
+	s, tp, err := TLS("test.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer s.Close()
+
+	i, err := random.Image(1024, 1)
+	if err != nil {
+		t.Fatalf("Unable to make image: %v", err)
+	}
+	rd, err := i.Digest()
+	if err != nil {
+		t.Fatalf("Unable to get image digest: %v", err)
+	}
+
+	d, err := name.NewDigest("test.com/foo@" + rd.String())
+	if err != nil {
+		t.Fatalf("Unable to parse digest: %v", err)
+	}
+	if err := remote.Write(d, i, remote.WithTransport(tp)); err != nil {
+		t.Fatalf("Unable to write image to remove: %s", err)
+	}
+}
