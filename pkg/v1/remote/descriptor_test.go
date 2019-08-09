5d07ccae338dd71d461b7514ce1cad26b0bc4300
jonjohnsonjr <jonjohnson@google.com>
2019-08-09 15:28:51 -0700

    Add mediatype to ErrSchema1 message (#455)

--- pkg/v1/remote/descriptor_test.go
+++ pkg/v1/remote/descriptor_test.go
@@ -21,6 +21,7 @@ import (
 	"net/url"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-containerregistry/pkg/v1/types"
 )
 
@@ -62,14 +63,26 @@ func TestGetSchema1(t *testing.T) {
 		t.Errorf("Descriptor.Digest = %q, expected %q", desc.Digest, fakeDigest)
 	}
 
+	want := `unsupported MediaType: "application/vnd.docker.distribution.manifest.v1+prettyjws", see https://github.com/google/go-containerregistry/issues/377`
 	// Should fail based on media type.
-	if _, err := desc.Image(); err != ErrSchema1 {
-		t.Errorf("Image() = %v, expected remote.ErrSchema1", err)
+	if _, err := desc.Image(); err != nil {
+		if _, ok := err.(*ErrSchema1); !ok {
+			t.Errorf("Image() = %v, expected remote.ErrSchema1", err)
+		}
+		if diff := cmp.Diff(want, err.Error()); diff != "" {
+			t.Errorf("Image() error message (-want +got) = %v", diff)
+		}
+	} else {
+		t.Errorf("Image() = %v, expected err", err)
 	}
 
 	// Should fail based on media type.
-	if _, err := desc.ImageIndex(); err != ErrSchema1 {
-		t.Errorf("ImageIndex() = %v, expected remote.ErrSchema1", err)
+	if _, err := desc.ImageIndex(); err != nil {
+		if _, ok := err.(*ErrSchema1); !ok {
+			t.Errorf("ImageImage() = %v, expected remote.ErrSchema1", err)
+		}
+	} else {
+		t.Errorf("ImageIndex() = %v, expected err", err)
 	}
 }
 
