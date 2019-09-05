e0cc78fd4835ee2989912b26efb4c8f0a4b61a91
jonjohnsonjr <jonjohnson@google.com>
2019-09-05 13:17:08 -0700

    Implement schema 1 copying (#509)
    Adds a new package, internal/legacy, where we can keep this without
    worrying about the API.

---
+++ pkg/internal/legacy/copy.go
@@ -0,0 +1,87 @@
+package legacy
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
+	"github.com/google/go-containerregistry/pkg/v1/types"
+)
+
+// CopySchema1 allows `[g]crane cp` to work with old images without adding
+// full support for schema 1 images to this package.
+func CopySchema1(desc *remote.Descriptor, srcRef, dstRef name.Reference, srcAuth, dstAuth authn.Authenticator) error {
+	m := schema1{}
+	if err := json.NewDecoder(bytes.NewReader(desc.Manifest)).Decode(&m); err != nil {
+		return err
+	}
+
+	for _, layer := range m.FSLayers {
+		src := fmt.Sprintf("%s@%s", srcRef.Context(), layer.BlobSum)
+		blobSrc, err := name.NewDigest(src)
+		if err != nil {
+			return err
+		}
+		dst := fmt.Sprintf("%s@%s", dstRef.Context(), layer.BlobSum)
+		blobDst, err := name.NewDigest(dst)
+		if err != nil {
+			return err
+		}
+
+		blob, err := remote.Layer(blobSrc, remote.WithAuth(srcAuth))
+		if err != nil {
+			return err
+		}
+
+		if err := remote.WriteLayer(blobDst, blob, remote.WithAuth(dstAuth)); err != nil {
+			return err
+		}
+	}
+
+	return putManifest(desc.Manifest, desc.MediaType, dstRef, dstAuth)
+}
+
+// TODO: perhaps expose this in remote?
+func putManifest(body []byte, mt types.MediaType, dstRef name.Reference, dstAuth authn.Authenticator) error {
+	reg := dstRef.Context().Registry
+	scopes := []string{dstRef.Scope(transport.PushScope)}
+	tr, err := transport.New(reg, dstAuth, http.DefaultTransport, scopes)
+	if err != nil {
+		return err
+	}
+	client := &http.Client{Transport: tr}
+
+	u := url.URL{
+		Scheme: dstRef.Context().Registry.Scheme(),
+		Host:   dstRef.Context().RegistryStr(),
+		Path:   fmt.Sprintf("/v2/%s/manifests/%s", dstRef.Context().RepositoryStr(), dstRef.Identifier()),
+	}
+
+	req, err := http.NewRequest(http.MethodPut, u.String(), bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", string(mt))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return transport.CheckError(resp, http.StatusOK, http.StatusCreated, http.StatusAccepted)
+}
+
+type fslayer struct {
+	BlobSum string `json:"blobSum"`
+}
+
+type schema1 struct {
+	FSLayers []fslayer `json:"fsLayers"`
+}
