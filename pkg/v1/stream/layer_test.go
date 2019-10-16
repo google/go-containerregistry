f2b0120cb80b78b47d62f608ea4761ffe14877f8
Javier Romero <jromero@pivotal.io>
2019-10-16 14:57:25 -0500

    Add compression option to tarball.Layer and stream.Layer
    Signed-off-by: Javier Romero <jromero@pivotal.io>

--- pkg/v1/stream/layer_test.go
+++ pkg/v1/stream/layer_test.go
@@ -84,6 +84,25 @@ func TestStreamVsBuffer(t *testing.T) {
 	} else if s != wantSize {
 		t.Errorf("stream Size got %d, want %d", s, wantSize)
 	}
+
+	// Test with different compression
+	l2 := NewLayer(newBlob(), WithCompressionLevel(2))
+	l2WantDigest := "sha256:c9afe7b0da6783232e463e12328cb306142548384accf3995806229c9a6a707f"
+	if c, err := l2.Compressed(); err != nil {
+		t.Errorf("Compressed: %v", err)
+	} else {
+		if _, err := io.Copy(ioutil.Discard, c); err != nil {
+			t.Errorf("error reading Compressed: %v", err)
+		}
+		if err := c.Close(); err != nil {
+			t.Errorf("Close: %v", err)
+		}
+	}
+	if d, err := l2.Digest(); err != nil {
+		t.Errorf("Digest: %v", err)
+	} else if d.String() != l2WantDigest {
+		t.Errorf("stream Digest got %q, want %q", d.String(), l2WantDigest)
+	}
 }
 
 func TestLargeStream(t *testing.T) {
