221517453cf931400e6607315045445644122692
jonjohnsonjr <jonjohnson@google.com>
2020-02-20 13:53:34 -0800

    Fix lint (#677)

--- pkg/internal/httptest/httptest.go
+++ pkg/internal/httptest/httptest.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package httptest provides a method for testing a TLS server a la net/http/httptest.
 package httptest
 
 import (
