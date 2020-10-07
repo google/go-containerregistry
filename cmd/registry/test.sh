51c2fb2d83e02d05ce7f9ca181588f426130db6a
Mark Terrel <mark@unbounded.systems>
2020-10-07 10:20:35 -0600

    Enable registry test shell script to run on Windows (Docker+Linux containers only)

--- cmd/registry/test.sh
+++ cmd/registry/test.sh
@@ -1,11 +1,46 @@
 #!/bin/bash
-set -ev
+set -ex
+
+CONTAINER_OS=$(docker info -f '{{ .OSType }}')
+
+# crane can run on a Windows system, but doesn't currently support pulling Windows
+# containers, so this test can only run if Docker is in Linux container mode.
+if [[ ${CONTAINER_OS} = "windows" ]]; then
+    set +x
+    echo [TEST SKIPPED] Windows containers are not yet supported by crane
+    exit
+fi
+
+function cleanup {
+    [[ -n $PID ]] && kill $PID
+    [[ -n $CTR ]] && docker stop $CTR
+    rm -f ubuntu.tar debiand.tar debianc.tar
+    docker rmi -f \
+        localhost:1338/debianc:latest \
+        localhost:1338/debiand:latest \
+        localhost:1338/ubuntuc:foo \
+        localhost:1338/ubuntud:latest \
+        || true
+}
+trap cleanup EXIT
+
+case "$OSTYPE" in
+    # On Windows, Docker runs in a VM, so a registry running on the Windows
+    # host is not accessible via localhost for `docker pull|push`.
+    win*|msys*|cygwin*)
+        docker run -d --rm -p 1338:5000 --name test-reg registry:2
+        CTR=test-reg
+        ;;
+
+    *)
+        registry &
+        PID=$!
+        ;;
+esac
 
 go install ./cmd/registry
 go install ./cmd/crane
 
-registry &
-PID=$!
 
 crane pull debian:latest debianc.tar
 crane push debianc.tar localhost:1338/debianc:latest
@@ -20,8 +55,3 @@ docker push localhost:1338/ubuntud:latest
 crane pull localhost:1338/ubuntud:latest ubuntu.tar
 crane push ubuntu.tar localhost:1338/ubuntuc:foo
 docker pull localhost:1338/ubuntuc:foo
-rm ubuntu.tar
-rm debiand.tar
-rm debianc.tar
-
-kill $PID
