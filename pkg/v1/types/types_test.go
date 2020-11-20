93b58dc0ebbff2cf010833d40e67a5e8c1468a29
Avi Deitcher <avi@deitcher.net>
2020-11-20 21:21:55 +0200

    add FindManifests (#828)
    Signed-off-by: Avi Deitcher <avi@deitcher.net>
    Co-authored-by: jonjohnsonjr <jonjohnson@google.com>
    Signed-off-by: Avi Deitcher <avi@deitcher.net>
    
    Co-authored-by: jonjohnsonjr <jonjohnson@google.com>

--- pkg/v1/types/types_test.go
+++ pkg/v1/types/types_test.go
@@ -34,3 +34,65 @@ func TestIsDistributable(t *testing.T) {
 		}
 	}
 }
+
+func TestIsImage(t *testing.T) {
+	for _, mt := range []MediaType{
+		OCIManifestSchema1, DockerManifestSchema2,
+	} {
+		if !mt.IsImage() {
+			t.Errorf("%s: should be image", mt)
+		}
+	}
+
+	for _, mt := range []MediaType{
+		OCIContentDescriptor,
+		OCIImageIndex,
+		OCIConfigJSON,
+		OCILayer,
+		OCIRestrictedLayer,
+		OCIUncompressedLayer,
+		OCIUncompressedRestrictedLayer,
+
+		DockerManifestList,
+		DockerLayer,
+		DockerConfigJSON,
+		DockerPluginConfig,
+		DockerForeignLayer,
+		DockerUncompressedLayer,
+	} {
+		if mt.IsImage() {
+			t.Errorf("%s: should not be image", mt)
+		}
+	}
+}
+
+func TestIsIndex(t *testing.T) {
+	for _, mt := range []MediaType{
+		OCIImageIndex, DockerManifestList,
+	} {
+		if !mt.IsIndex() {
+			t.Errorf("%s: should be index", mt)
+		}
+	}
+
+	for _, mt := range []MediaType{
+		OCIContentDescriptor,
+		OCIConfigJSON,
+		OCILayer,
+		OCIRestrictedLayer,
+		OCIUncompressedLayer,
+		OCIUncompressedRestrictedLayer,
+		OCIManifestSchema1,
+
+		DockerManifestSchema2,
+		DockerLayer,
+		DockerConfigJSON,
+		DockerPluginConfig,
+		DockerForeignLayer,
+		DockerUncompressedLayer,
+	} {
+		if mt.IsIndex() {
+			t.Errorf("%s: should not be index", mt)
+		}
+	}
+}
