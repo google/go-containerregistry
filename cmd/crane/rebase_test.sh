c78a56e0053d309253e991c52006dc7c1525fa96
Jason Hall <jasonhall@redhat.com>
2021-08-04 11:29:17 -0400

    rm rebase_cloudbuild.yaml, exercise :rebased tagging logic in rebase_test.sh

--- cmd/crane/rebase_test.sh
+++ cmd/crane/rebase_test.sh
@@ -20,36 +20,35 @@ sleep 1  # Wait for registry to be up.
 
 # Create an image localhost:1338/base containing a.txt
 echo a > ${tmp}/a.txt
-./crane append -f <(tar -f - -c ${tmp}) -t localhost:1338/base
+old_base=$(./crane append -f <(tar -f - -c ${tmp}) -t localhost:1338/base)
 rm ${tmp}/a.txt
 
 # Append to that image localhost:1338/rebaseme
 echo top > ${tmp}/top.txt
-./crane append -f <(tar -f - -c ${tmp}) -b localhost:1338/base -t localhost:1338/rebaseme
+orig=$(./crane append -f <(tar -f - -c ${tmp}) -b ${old_base} -t localhost:1338/rebaseme)
 rm ${tmp}/top.txt
 
 # Annotate that image as the base image (by ref and digest)
 # TODO: do this with a flag to --append
-./crane mutate localhost:1338/rebaseme \
+orig=$(./crane mutate ${orig} \
   --annotation org.opencontainers.image.base.name=localhost:1338/base \
-  --annotation org.opencontainers.image.base.digest=$(./crane digest localhost:1338/base) \
-  --tag localhost:1338/rebaseme
+  --annotation org.opencontainers.image.base.digest=$(./crane digest localhost:1338/base))
 
 # Update localhost:1338/base containing b.txt
 echo b > ${tmp}/b.txt
-./crane append -f <(tar -f - -c ${tmp}) -t localhost:1338/base
+new_base=$(./crane append -f <(tar -f - -c ${tmp}) -t localhost:1338/base)
 rm ${tmp}/b.txt
 
 # Rebase using annotations
-rebased=$(./crane rebase localhost:1338/rebaseme)
+rebased=$(./crane rebase ${orig})
 
 # List files in the rebased image.
 ./crane export ${rebased} - | tar -tvf -
 
-# Extract b.txt out of the rebased image
+# Extract b.txt out of the rebased image.
 ./crane export ${rebased} - | tar -Oxf - ${tmp:1}/b.txt
 
-# Extract top.txt out of the rebased image
+# Extract top.txt out of the rebased image.
 ./crane export ${rebased} - | tar -Oxf - ${tmp:1}/top.txt
 
 # a.txt is _not_ in the rebased image.
@@ -61,4 +60,3 @@ if [[ $code -ne 1 ]]; then
   echo "a.txt was found in rebased image"
   exit 1
 fi
-
