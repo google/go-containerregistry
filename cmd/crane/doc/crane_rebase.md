de8aff85c9090d37ed6fe436d696f3a820e5c767
Jason Hall <jasonhall@redhat.com>
2021-08-16 14:25:56 -0400

    Implement annotation-based rebase hints (#960)
    * Implement annotation-based rebase hints
    
    `crane rebase` now inspects the original image for annotations to
    identify its old base and new base images. If those are found, and
    --old_base and --new_base flags aren't specified, those will be used.
    
    If the --rebased flag is not passed, the rebased image will be the
    tagged with the original image reference; if it was originally specified
    by digest, the rebased image will be tagged with :rebased (instead of
    stripping the digest and pushing to :latest)
    
    `crane rebase` now prints the full pushed image reference, instead of
    just the digest (aiding embedding in other bash pipelines), and adds
    annotations to aid future rebasings.
    
    This also adds a bash test that covers the rebase case for detected base
    image information.
    
    * rm rebase_cloudbuild.yaml, exercise :rebased tagging logic in rebase_test.sh
    
    * remove debug logging
    
    * use crane.Pull to honor flags
    
    * pick up improvements from crane flatten
    
    * keep --rebased for backcompat
    
    * review feedback
    
    * update codegen
    
    * remove Verify GitHub Action; it did nothing that Presubmit didn't do
    
    * update rebase.md docs

--- cmd/crane/doc/crane_rebase.md
+++ cmd/crane/doc/crane_rebase.md
@@ -12,8 +12,9 @@ crane rebase [flags]
   -h, --help              help for rebase
       --new_base string   New base image to insert
       --old_base string   Old base image to remove
-      --original string   Original image to rebase
-      --rebased string    Tag to apply to rebased image
+      --original string   Original image to rebase (DEPRECATED: use positional arg instead)
+      --rebased string    Tag to apply to rebased image (DEPRECATED: use --tag)
+  -t, --tag string        Tag to apply to rebased image
 ```
 
 ### Options inherited from parent commands
---
+++ cmd/crane/rebase_test.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -ex
+
+tmp=$(mktemp -d)
+
+go install ./cmd/registry
+go build -o ./crane ./cmd/crane
+
+# Start a local registry.
+registry &
+PID=$!
+function cleanup {
+  kill $PID
+  rm -r ${tmp}
+  rm ./crane
+}
+trap cleanup EXIT
+
+sleep 1  # Wait for registry to be up.
+
+# Create an image localhost:1338/base containing a.txt
+echo a > ${tmp}/a.txt
+old_base=$(./crane append -f <(tar -f - -c ${tmp}) -t localhost:1338/base)
+rm ${tmp}/a.txt
+
+# Append to that image localhost:1338/rebaseme
+echo top > ${tmp}/top.txt
+orig=$(./crane append -f <(tar -f - -c ${tmp}) -b ${old_base} -t localhost:1338/rebaseme)
+rm ${tmp}/top.txt
+
+# Annotate that image as the base image (by ref and digest)
+# TODO: do this with a flag to --append
+orig=$(./crane mutate ${orig} \
+  --annotation org.opencontainers.image.base.name=localhost:1338/base \
+  --annotation org.opencontainers.image.base.digest=$(./crane digest localhost:1338/base))
+
+# Update localhost:1338/base containing b.txt
+echo b > ${tmp}/b.txt
+new_base=$(./crane append -f <(tar -f - -c ${tmp}) -t localhost:1338/base)
+rm ${tmp}/b.txt
+
+# Rebase using annotations
+rebased=$(./crane rebase ${orig})
+
+# List files in the rebased image.
+./crane export ${rebased} - | tar -tvf -
+
+# Extract b.txt out of the rebased image.
+./crane export ${rebased} - | tar -Oxf - ${tmp:1}/b.txt
+
+# Extract top.txt out of the rebased image.
+./crane export ${rebased} - | tar -Oxf - ${tmp:1}/top.txt
+
+# a.txt is _not_ in the rebased image.
+set +e
+./crane export ${rebased} - | tar -Oxf - ${tmp:1}/a.txt  # this should fail
+code=$?
+echo "finding a.txt exited ${code}"
+if [[ $code -ne 1 ]]; then
+  echo "a.txt was found in rebased image"
+  exit 1
+fi
