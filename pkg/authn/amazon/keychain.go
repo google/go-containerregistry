34091eb21a75ecd6cf763b264099ef1a23234c17
Jason Hall <jasonhall@redhat.com>
2022-01-03 12:07:15 -0500

    rebase and use authn.NewFromHelper

--- pkg/authn/amazon/keychain.go
+++ pkg/authn/amazon/keychain.go
@@ -15,39 +15,10 @@
 package amazon
 
 import (
+	ecr "github.com/awslabs/amazon-ecr-credential-helper/ecr-login"
 	"github.com/awslabs/amazon-ecr-credential-helper/ecr-login/api"
 	"github.com/google/go-containerregistry/pkg/authn"
 )
 
 // Keychain exports an instance of the amazon Keychain.
-var Keychain authn.Keychain = amazonKeychain{}
-
-type amazonKeychain struct{}
-
-// Resolve implements authn.Keychain a la docker-credential-ecr-login.
-func (amazonKeychain) Resolve(target authn.Resource) (authn.Authenticator, error) {
-	reg, err := api.ExtractRegistry(target.String())
-	if err != nil {
-		return authn.Anonymous, nil
-	}
-
-	cf := api.DefaultClientFactory{}
-	var client api.Client
-	if reg.FIPS {
-		client, err = cf.NewClientWithFipsEndpoint(reg.Region)
-		if err != nil {
-			return authn.Anonymous, nil
-		}
-	} else {
-		client = cf.NewClientFromRegion(reg.Region)
-	}
-
-	auth, err := client.GetCredentials(target.String())
-	if err != nil {
-		return authn.Anonymous, nil
-	}
-	return &authn.Basic{
-		Username: auth.Username,
-		Password: auth.Password,
-	}, nil
-}
+var Keychain authn.Keychain = authn.NewFromHelper(ecr.ECRHelper{api.DefaultClientFactory{}})
