22acc70bd283edbc56dbf914324fd20b59bf36e2
dprotaso <dprotaso@gmail.com>
2022-04-13 23:13:14 -0400

    authn.kubernetes.Resolve now behaves exactly like Kubernetes
    Prior we weren't matching host names and partial paths properly
    
    This was breaking Gitlab and Azure tag to digest resolution in Knative

--- pkg/authn/kubernetes/keychain_test.go
+++ pkg/authn/kubernetes/keychain_test.go
@@ -16,17 +16,60 @@ package kubernetes
 
 import (
 	"context"
-	"encoding/base64"
+	"crypto/md5"
+	"encoding/json"
 	"fmt"
-	"reflect"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	fakeclient "k8s.io/client-go/kubernetes/fake"
 )
+
+var dockerSecretTypes = []secretType{
+	dockerConfigJSONSecretType,
+	dockerCfgSecretType,
+}
+
+type secretType struct {
+	name    corev1.SecretType
+	key     string
+	marshal func(t *testing.T, registry string, auth authn.AuthConfig) []byte
+}
+
+func (s *secretType) Create(t *testing.T, namespace, name string, registry string, auth authn.AuthConfig) *corev1.Secret {
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Type: s.name,
+		Data: map[string][]byte{
+			s.key: s.marshal(t, registry, auth),
+		},
+	}
+}
+
+var dockerConfigJSONSecretType = secretType{
+	name: corev1.SecretTypeDockerConfigJson,
+	key:  corev1.DockerConfigJsonKey,
+	marshal: func(t *testing.T, target string, auth authn.AuthConfig) []byte {
+		return toJSON(t, dockerConfigJSON{
+			Auths: map[string]authn.AuthConfig{target: auth},
+		})
+	},
+}
+
+var dockerCfgSecretType = secretType{
+	name: corev1.SecretTypeDockercfg,
+	key:  corev1.DockerConfigKey,
+	marshal: func(t *testing.T, target string, auth authn.AuthConfig) []byte {
+		return toJSON(t, map[string]authn.AuthConfig{target: auth})
+	},
+}
 
 func TestAnonymousFallback(t *testing.T) {
 	client := fakeclient.NewSimpleClientset(&corev1.ServiceAccount{
@@ -41,18 +84,7 @@ func TestAnonymousFallback(t *testing.T) {
 		t.Errorf("New() = %v", err)
 	}
 
-	reg, err := name.NewRegistry("fake.registry.io", name.WeakValidation)
-	if err != nil {
-		t.Errorf("NewRegistry() = %v", err)
-	}
-
-	auth, err := kc.Resolve(reg)
-	if err != nil {
-		t.Errorf("Resolve(%v) = %v", reg, err)
-	}
-	if got, want := auth, authn.Anonymous; got != want {
-		t.Errorf("Resolve() = %v, want %v", got, want)
-	}
+	testResolve(t, kc, registry(t, "fake.registry.io"), authn.Anonymous)
 }
 
 func TestAttachedServiceAccount(t *testing.T) {
@@ -65,19 +97,12 @@ func TestAttachedServiceAccount(t *testing.T) {
 		ImagePullSecrets: []corev1.LocalObjectReference{{
 			Name: "secret",
 		}},
-	}, &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "secret",
-			Namespace: "ns",
-		},
-		Type: corev1.SecretTypeDockercfg,
-		Data: map[string][]byte{
-			corev1.DockerConfigKey: []byte(
-				fmt.Sprintf(`{"fake.registry.io": {"username": "%s", "password": "%s"}}`,
-					username, password),
-			),
-		},
-	})
+	},
+		dockerCfgSecretType.Create(t, "ns", "secret", "fake.registry.io", authn.AuthConfig{
+			Username: username,
+			Password: password,
+		}),
+	)
 
 	kc, err := New(context.Background(), client, Options{
 		Namespace:          "ns",
@@ -87,203 +112,242 @@ func TestAttachedServiceAccount(t *testing.T) {
 		t.Fatalf("New() = %v", err)
 	}
 
-	reg, err := name.NewRegistry("fake.registry.io", name.WeakValidation)
-	if err != nil {
-		t.Errorf("NewRegistry() = %v", err)
+	testResolve(t, kc, registry(t, "fake.registry.io"),
+		&authn.Basic{Username: username, Password: password})
+}
+
+// Prioritze picking the first secret
+func TestSecretPriority(t *testing.T) {
+	secrets := []corev1.Secret{
+		*dockerCfgSecretType.Create(t, "ns", "secret", "fake.registry.io", authn.AuthConfig{
+			Username: "user", Password: "pass",
+		}),
+		*dockerCfgSecretType.Create(t, "ns", "secret-2", "fake.registry.io", authn.AuthConfig{
+			Username: "anotherUser", Password: "anotherPass",
+		}),
 	}
 
-	auth, err := kc.Resolve(reg)
+	kc, err := NewFromPullSecrets(context.Background(), secrets)
 	if err != nil {
-		t.Errorf("Resolve(%v) = %v", reg, err)
+		t.Fatalf("NewFromPullSecrets() = %v", err)
+	}
+
+	expectedAuth := &authn.Basic{Username: "user", Password: "pass"}
+	testResolve(t, kc, registry(t, "fake.registry.io"), expectedAuth)
+}
+
+func TestResolveTargets(t *testing.T) {
+	// Iterate over target types
+	targetTypes := []authn.Resource{
+		registry(t, "fake.registry.io"),
+		repo(t, "fake.registry.io/repo"),
+	}
+
+	for _, secretType := range dockerSecretTypes {
+		for _, target := range targetTypes {
+			// Drop the .
+			testName := secretType.key[1:] + "_" + target.String()
+
+			t.Run(testName, func(t *testing.T) {
+				auth := authn.AuthConfig{
+					Password: fmt.Sprintf("%x", md5.Sum([]byte(t.Name()))),
+					Username: "user" + fmt.Sprintf("%x", md5.Sum([]byte(t.Name()))),
+				}
+
+				kc, err := NewFromPullSecrets(context.Background(), []corev1.Secret{
+					*secretType.Create(t, "ns", "secret", target.String(), auth),
+				})
+
+				if err != nil {
+					t.Fatalf("New() = %v", err)
+				}
+				authenticator := &authn.Basic{Username: auth.Username, Password: auth.Password}
+				testResolve(t, kc, target, authenticator)
+			})
+		}
+	}
+}
+
+func TestAuthWithScheme(t *testing.T) {
+	auth := authn.AuthConfig{
+		Password: "password",
+		Username: "username",
+	}
+
+	kc, err := NewFromPullSecrets(context.Background(), []corev1.Secret{
+		*dockerConfigJSONSecretType.Create(t, "ns", "secret", "https://fake.registry.io", auth),
+	})
+
+	if err != nil {
+		t.Fatalf("New() = %v", err)
+	}
+	authenticator := &authn.Basic{Username: auth.Username, Password: auth.Password}
+	testResolve(t, kc, registry(t, "fake.registry.io"), authenticator)
+	testResolve(t, kc, repo(t, "fake.registry.io/repo"), authenticator)
+}
+
+func TestAuthWithPorts(t *testing.T) {
+	auth := authn.AuthConfig{
+		Password: "password",
+		Username: "username",
+	}
+
+	kc, err := NewFromPullSecrets(context.Background(), []corev1.Secret{
+		*dockerConfigJSONSecretType.Create(t, "ns", "secret", "fake.registry.io:5000", auth),
+	})
+
+	if err != nil {
+		t.Fatalf("New() = %v", err)
+	}
+	authenticator := &authn.Basic{Username: auth.Username, Password: auth.Password}
+	testResolve(t, kc, registry(t, "fake.registry.io:5000"), authenticator)
+	testResolve(t, kc, repo(t, "fake.registry.io:5000/repo"), authenticator)
+
+	// Non-matching ports should return Anonymous
+	testResolve(t, kc, registry(t, "fake.registry.io:1000"), authn.Anonymous)
+	testResolve(t, kc, repo(t, "fake.registry.io:1000/repo"), authn.Anonymous)
+}
+
+func TestAuthPathMatching(t *testing.T) {
+	rootAuth := authn.AuthConfig{Username: "root", Password: "root"}
+	nestedAuth := authn.AuthConfig{Username: "nested", Password: "nested"}
+	leafAuth := authn.AuthConfig{Username: "leaf", Password: "leaf"}
+	partialAuth := authn.AuthConfig{Username: "partial", Password: "partial"}
+
+	kc, err := NewFromPullSecrets(context.Background(), []corev1.Secret{
+		*dockerConfigJSONSecretType.Create(t, "ns", "secret-1", "fake.registry.io", rootAuth),
+		*dockerConfigJSONSecretType.Create(t, "ns", "secret-2", "fake.registry.io/nested", nestedAuth),
+		*dockerConfigJSONSecretType.Create(t, "ns", "secret-3", "fake.registry.io/nested/repo", leafAuth),
+		*dockerConfigJSONSecretType.Create(t, "ns", "secret-4", "fake.registry.io/par", partialAuth),
+	})
+
+	if err != nil {
+		t.Fatalf("New() = %v", err)
+	}
+	testResolve(t, kc, registry(t, "fake.registry.io"), authn.FromConfig(rootAuth))
+	testResolve(t, kc, repo(t, "fake.registry.io/nested"), authn.FromConfig(nestedAuth))
+	testResolve(t, kc, repo(t, "fake.registry.io/nested/repo"), authn.FromConfig(leafAuth))
+	testResolve(t, kc, repo(t, "fake.registry.io/nested/repo/dirt"), authn.FromConfig(leafAuth))
+	testResolve(t, kc, repo(t, "fake.registry.io/partial"), authn.FromConfig(partialAuth))
+}
+
+func TestAuthHostNameVariations(t *testing.T) {
+	rootAuth := authn.AuthConfig{Username: "root", Password: "root"}
+	subdomainAuth := authn.AuthConfig{Username: "sub", Password: "sub"}
+
+	kc, err := NewFromPullSecrets(context.Background(), []corev1.Secret{
+		*dockerConfigJSONSecretType.Create(t, "ns", "secret-1", "fake.registry.io", rootAuth),
+		*dockerConfigJSONSecretType.Create(t, "ns", "secret-2", "1.fake.registry.io", subdomainAuth),
+	})
+
+	if err != nil {
+		t.Fatalf("New() = %v", err)
+	}
+
+	testResolve(t, kc, registry(t, "fake.registry.io"), authn.FromConfig(rootAuth))
+	testResolve(t, kc, registry(t, "1.fake.registry.io"), authn.FromConfig(subdomainAuth))
+
+	// Unrecognized subdomain uses Anonymous
+	testResolve(t, kc, registry(t, "2.fake.registry.io"), authn.Anonymous)
+}
+
+func TestAuthSpecialPathsIgnored(t *testing.T) {
+	auth := authn.AuthConfig{Username: "root", Password: "root"}
+	auth2 := authn.AuthConfig{Username: "root2", Password: "root2"}
+
+	kc, err := NewFromPullSecrets(context.Background(), []corev1.Secret{
+		// Note the paths need a trailing '/'
+		*dockerConfigJSONSecretType.Create(t, "ns", "secret-1", "https://fake.registry.io/v1/", auth),
+		*dockerConfigJSONSecretType.Create(t, "ns", "secret-2", "https://fake2.registry.io/v2/", auth2),
+	})
+
+	if err != nil {
+		t.Fatalf("New() = %v", err)
+	}
+
+	testResolve(t, kc, registry(t, "fake.registry.io"), authn.FromConfig(auth))
+	testResolve(t, kc, repo(t, "fake.registry.io/repo"), authn.FromConfig(auth))
+	testResolve(t, kc, registry(t, "fake2.registry.io"), authn.FromConfig(auth2))
+	testResolve(t, kc, repo(t, "fake2.registry.io/repo"), authn.FromConfig(auth2))
+}
+
+func TestAuthDockerRegistry(t *testing.T) {
+	auth := authn.AuthConfig{Username: "root", Password: "root"}
+	kc, err := NewFromPullSecrets(context.Background(), []corev1.Secret{
+		*dockerConfigJSONSecretType.Create(t, "ns", "secret", "index.docker.io", auth),
+	})
+
+	if err != nil {
+		t.Fatalf("New() = %v", err)
+	}
+
+	testResolve(t, kc, repo(t, "ubuntu"), authn.FromConfig(auth))
+	testResolve(t, kc, repo(t, "knative/serving"), authn.FromConfig(auth))
+}
+
+func TestAuthWithGlobs(t *testing.T) {
+	auth := authn.AuthConfig{Username: "root", Password: "root"}
+	kc, err := NewFromPullSecrets(context.Background(), []corev1.Secret{
+		*dockerConfigJSONSecretType.Create(t, "ns", "secret", "*.registry.io", auth),
+	})
+
+	if err != nil {
+		t.Fatalf("New() = %v", err)
+	}
+
+	testResolve(t, kc, registry(t, "fake.registry.io"), authn.FromConfig(auth))
+	testResolve(t, kc, repo(t, "fake.registry.io/repo"), authn.FromConfig(auth))
+	testResolve(t, kc, registry(t, "blah.registry.io"), authn.FromConfig(auth))
+	testResolve(t, kc, repo(t, "blah.registry.io/repo"), authn.FromConfig(auth))
+}
+
+func testResolve(t *testing.T, kc authn.Keychain, target authn.Resource, expectedAuth authn.Authenticator) {
+	t.Helper()
+
+	auth, err := kc.Resolve(target)
+	if err != nil {
+		t.Errorf("Resolve(%v) = %v", target, err)
 	}
 	got, err := auth.Authorization()
 	if err != nil {
 		t.Errorf("Authorization() = %v", err)
 	}
-	want, err := (&authn.Basic{Username: username, Password: password}).Authorization()
+	want, err := expectedAuth.Authorization()
 	if err != nil {
 		t.Errorf("Authorization() = %v", err)
 	}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("Resolve() = %v, want %v", got, want)
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Error("Resolve() diff (-want, +got)\n", diff)
 	}
 }
 
-func TestImagePullSecrets(t *testing.T) {
-	username, password := "foo", "bar"
-	specificUser, specificPass := "very", "specific"
-	client := fakeclient.NewSimpleClientset(&corev1.ServiceAccount{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "default",
-			Namespace: "ns",
-		},
-	}, &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "secret",
-			Namespace: "ns",
-		},
-		Type: corev1.SecretTypeDockercfg,
-		Data: map[string][]byte{
-			corev1.DockerConfigKey: []byte(
-				fmt.Sprintf(`{"fake.registry.io": {"auth": %q}, "fake.registry.io/more/specific": {"auth": %q}}`,
-					base64.StdEncoding.EncodeToString([]byte(username+":"+password)),
-					base64.StdEncoding.EncodeToString([]byte(specificUser+":"+specificPass))),
-			),
-		},
-	})
+func toJSON(t *testing.T, obj interface{}) []byte {
+	t.Helper()
 
-	kc, err := New(context.Background(), client, Options{
-		Namespace:        "ns",
-		ImagePullSecrets: []string{"secret"},
-	})
+	bites, err := json.Marshal(obj)
+
 	if err != nil {
-		t.Fatalf("New() = %v", err)
+		t.Fatal("unable to json marshal", err)
 	}
-
-	repo, err := name.NewRepository("fake.registry.io/more/specific", name.WeakValidation)
-	if err != nil {
-		t.Errorf("NewRegistry() = %v", err)
-	}
-
-	for _, tc := range []struct {
-		name   string
-		auth   authn.Authenticator
-		target authn.Resource
-	}{{
-		name:   "registry",
-		auth:   &authn.Basic{Username: username, Password: password},
-		target: repo.Registry,
-	}, {
-		name:   "repo",
-		auth:   &authn.Basic{Username: specificUser, Password: specificPass},
-		target: repo,
-	}} {
-		t.Run(tc.name, func(t *testing.T) {
-			tc := tc
-			auth, err := kc.Resolve(tc.target)
-			if err != nil {
-				t.Errorf("Resolve(%v) = %v", tc.target, err)
-			}
-			got, err := auth.Authorization()
-			if err != nil {
-				t.Errorf("Authorization() = %v", err)
-			}
-			want, err := tc.auth.Authorization()
-			if err != nil {
-				t.Errorf("Authorization() = %v", err)
-			}
-			if !reflect.DeepEqual(got, want) {
-				t.Errorf("Resolve() = %v, want %v", got, want)
-			}
-		})
-	}
+	return bites
 }
 
-func TestFromPullSecrets(t *testing.T) {
-	username, password := "foo", "bar"
-	specificUser, specificPass := "very", "specific"
+func registry(t *testing.T, registry string) authn.Resource {
+	t.Helper()
 
-	pullSecrets := []corev1.Secret{{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "secret",
-			Namespace: "ns",
-		},
-		Type: corev1.SecretTypeDockercfg,
-		Data: map[string][]byte{
-			corev1.DockerConfigKey: []byte(
-				fmt.Sprintf(`
-					{
-						"fake.registry.io": {"auth": %q},
-						"fake.registry.io/more/specific": {"auth": %q},
-						"http://fake.scheme-registry.io": {"auth": %q},
-						"https://fake.scheme-registry.io/more/specific": {"auth": %q},
-						"https://index.docker.io/v1/": {"auth": %q}
-					}`,
-					base64.StdEncoding.EncodeToString([]byte(username+":"+password)),
-					base64.StdEncoding.EncodeToString([]byte(specificUser+":"+specificPass)),
-					base64.StdEncoding.EncodeToString([]byte(username+":"+password)),
-					base64.StdEncoding.EncodeToString([]byte(specificUser+":"+specificPass)),
-					base64.StdEncoding.EncodeToString([]byte(username+":"+password))),
-			),
-		},
-	}, {
-		// Check that a subsequent Secret that matches the registry is
-		// _not_ used; i.e., first matching Secret wins.
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "secret-2",
-			Namespace: "ns",
-		},
-		Type: corev1.SecretTypeDockercfg,
-		Data: map[string][]byte{
-			corev1.DockerConfigKey: []byte(
-				fmt.Sprintf(`{"fake.registry.io": {"auth": %q}}`,
-					base64.StdEncoding.EncodeToString([]byte("anotherUser:anotherPass"))),
-			),
-		},
-	}}
-
-	kc, err := NewFromPullSecrets(context.Background(), pullSecrets)
+	reg, err := name.NewRegistry(registry, name.WeakValidation)
 	if err != nil {
-		t.Fatalf("NewFromPullSecrets() = %v", err)
+		t.Fatal("failed to create registry", err)
 	}
+	return reg
+}
 
-	repo, err := name.NewRepository("fake.registry.io/more/specific", name.WeakValidation)
+func repo(t *testing.T, repository string) authn.Resource {
+	t.Helper()
+
+	repo, err := name.NewRepository(repository, name.WeakValidation)
 	if err != nil {
-		t.Errorf("NewRegistry() = %v", err)
+		t.Fatal("failed to create repo", err)
 	}
-
-	schemeRepo, err := name.NewRepository("fake.scheme-registry.io/more/specific", name.WeakValidation)
-	if err != nil {
-		t.Errorf("NewRegistry() = %v", err)
-	}
-
-	dockerHubRepo, err := name.NewRepository("nginx", name.WeakValidation)
-	if err != nil {
-		t.Errorf("NewRegistry() = %v", err)
-	}
-
-	for _, tc := range []struct {
-		name   string
-		auth   authn.Authenticator
-		target authn.Resource
-	}{{
-		name:   "registry",
-		auth:   &authn.Basic{Username: username, Password: password},
-		target: repo.Registry,
-	}, {
-		name:   "repo",
-		auth:   &authn.Basic{Username: specificUser, Password: specificPass},
-		target: repo,
-	}, {
-		name:   "registry with scheme",
-		auth:   &authn.Basic{Username: username, Password: password},
-		target: schemeRepo.Registry,
-	}, {
-		name:   "repo with scheme",
-		auth:   &authn.Basic{Username: specificUser, Password: specificPass},
-		target: schemeRepo,
-	}, {
-		name:   "docker hub repo",
-		auth:   &authn.Basic{Username: username, Password: password},
-		target: dockerHubRepo,
-	}} {
-		t.Run(tc.name, func(t *testing.T) {
-			tc := tc
-			auth, err := kc.Resolve(tc.target)
-			if err != nil {
-				t.Errorf("Resolve(%v) = %v", tc.target, err)
-			}
-			got, err := auth.Authorization()
-			if err != nil {
-				t.Errorf("Authorization() = %v", err)
-			}
-			want, err := tc.auth.Authorization()
-			if err != nil {
-				t.Errorf("Authorization() = %v", err)
-			}
-			if !reflect.DeepEqual(got, want) {
-				t.Errorf("Resolve() = %v, want %v", got, want)
-			}
-		})
-	}
+	return repo
 }
