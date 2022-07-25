8f2ec38b6a6da359bac5b5626f434a52bb196e98
João Pereira <joaod@vmware.com>
2022-07-25 10:38:52 -0500

    Add flag to registry to allow new features to be used in command line
    Signed-off-by: João Pereira <joaod@vmware.com>

--- cmd/registry/main.go
+++ cmd/registry/main.go
@@ -10,12 +10,33 @@ import (
 )
 
 var port = flag.Int("port", 1338, "port to run registry on")
+var storage = flag.String("storage", "memory", "Option [memory, disk], default value is memory")
+var diskLocation = flag.String("path", "", "Required when selecting disk storage. Path where the registry will store the blobs")
+var splitRepositories = flag.Bool("split-repositories", false, "Separate blob storage per repository. This feature will ensures that each repository only has access to blobs inside the repository space.")
 
 func main() {
 	flag.Parse()
+
+	var opts []registry.Option
+	switch *storage {
+	case "memory":
+	case "disk":
+		if *diskLocation == "" {
+			log.Fatal("When storage is set to 'disk' a location has to be provided via the flag '--path'")
+		}
+
+		opts = append(opts, registry.DiskBlobStorage(*diskLocation))
+	default:
+		log.Fatalf("Option '%s' is not valid for storage flag. Choose 'memory' or 'disk'", *storage)
+	}
+
+	if *splitRepositories {
+		opts = append(opts, registry.SplitBlobsByRepository())
+	}
+
 	s := &http.Server{
 		Addr:    fmt.Sprintf(":%d", *port),
-		Handler: registry.New(),
+		Handler: registry.New(opts...),
 	}
 	log.Fatal(s.ListenAndServe())
 }
